0c1353bcbe5a8f15828bb3d4c68ecf6429a521aa
Clement <adresse.utilitaire@outlook.fr>
2024-04-25 14:56:45 +0200

    Ajout models et predictions (je crois que se sont les mêmes) et début de maj sur le traitement pca, chi2

--- src/machine_learning.py
+++ src/machine_learning.py
@@ -2,6 +2,11 @@
 import pandas as pd
 from scipy.io import arff
 from src.constantes import ConstantesArff as coarff
+from lazypredict.Supervised import LazyClassifier
+from sklearn.preprocessing import LabelEncoder
+import numpy as np
+from sklearn.decomposition import PCA
+from sklearn.feature_selection import SelectKBest, chi2
 
 
 class ArffToDf:
@@ -12,6 +17,7 @@ class ArffToDf:
     def dataframe(self):
         df_train = pd.DataFrame(self.arff_file_train[0])
         df_test = pd.DataFrame(self.arff_file_test[0])
+        self.colonnes = df_train.columns.tolist()
         return df_train, df_test
 
     def x_y_split(self, dataframe):
@@ -19,34 +25,56 @@ class ArffToDf:
         y = dataframe.iloc[:, -1]
         return X, y
 
-    def encode_colonnes(self, dataframe):
+    def encode_X(self, dataframe):
         df_object = dataframe.select_dtypes(include='object')
         df_encode = pd.get_dummies(dataframe, columns=df_object.columns)
+        df_encode.columns = df_encode.columns.str.replace("b'", "")
+        df_encode.columns = df_encode.columns.str.replace("'", "")
         return df_encode
 
+    def encode_y(self, y):
+        le = LabelEncoder()
+        y = le.fit_transform(y)
+        return y
+
+    def egalisage_colonne(self, X_train, X_test):
+        missing_columns = set(X_train.columns) - set(X_test.columns)
+        for column in missing_columns:
+            X_test[column] = 0
+        X_test = X_test[X_train.columns]  # permet de remettre les colonnes dans le bon ordre
+        return X_test
+
+    def conversion_numpy(self, X_train, X_test):
+        X_train = X_train.to_numpy()
+        X_test = X_test.to_numpy()
+        return X_train, X_test
 
     def process(self):
         df_train, df_test = self.dataframe()
         X_train, y_train = self.x_y_split(df_train)
         X_test, y_test = self.x_y_split(df_test)
-        X_train = self.encode_colonnes(X_train)
-        X_test = self.encode_colonnes(X_test)
-        return X_train, y_train, X_test, y_test
+        X_train = self.encode_X(X_train)
+        X_test = self.encode_X(X_test)
+        y_train = self.encode_y(y_train)
+        y_test = self.encode_y(y_test)
+        X_test = self.egalisage_colonne(X_train, X_test)
+        return X_train, y_train, X_test, y_test, self.colonnes
 
 
+X_train, y_train, X_test, y_test, colonnes = ArffToDf().process()
+clf = LazyClassifier(verbose=True, ignore_warnings=False, custom_metric=None)
+models, predictions = clf.fit(X_train, X_test, y_train, y_test)
 
 
-X_train, y_train, X_test, y_test = ArffToDf().process()
-
-
-class LeMonstre:
-    def __init__(self):
-        pass
+class TraitementDonnees:
+    def __init__(self, X):
+        self.X = X
 
     def pca_election(self):
-        pass
+        pca = PCA()
 
-    def chi_selection(self):
-        pass
+    def chi_selection(self, X, y):
+        chi2_selector = SelectKBest(chi2, k=10)
+        X_kbest = chi2_selector.fit_transform(X, y)
 
 
